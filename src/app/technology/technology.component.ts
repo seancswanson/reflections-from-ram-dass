ee9a8c0359d8041f2dcb6536ef8a4366339e4cbc
Sean Swanson <sean-swan@hotmail.com>
2021-03-29 18:39:15 -0700

    Added icons and links to all technologies used in the project and fleshed out that info component further. Getting ready for deploy.

--- src/app/technology/technology.component.ts
+++ src/app/technology/technology.component.ts
@@ -9,7 +9,7 @@ export class TechnologyComponent implements OnInit {
   technologiesUsed = [
     {
       name: "HTML5",
-      iconUrl: "../../assets/devicons/css3-original.png",
+      iconUrl: "../../assets/devicons/html5-original.png",
       resourceUrl:
         "https://www.freecodecamp.org/news/best-html-html5-tutorial/",
     },
@@ -21,34 +21,43 @@ export class TechnologyComponent implements OnInit {
     },
     {
       name: "SCSS",
-      iconUrl: "../../assets/devicons/css3-original.png",
-      resourceUrl:
-        "https://www.freecodecamp.org/news/best-css-and-css3-tutorial/",
+      iconUrl: "../../assets/devicons/sass-original.png",
+      resourceUrl: "https://sass-lang.com/",
     },
     {
       name: "JavaScript",
-      iconUrl: "../../assets/devicons/css3-original.png",
+      iconUrl: "../../assets/devicons/javascript-original.png",
       resourceUrl: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
     },
     {
       name: "Angular",
-      iconUrl: "../../assets/devicons/css3-original.png",
+      iconUrl: "../../assets/devicons/angularjs-original.png",
       resourceUrl: "https://angular.io/",
     },
     {
       name: "TypeScript",
-      iconUrl: "../../assets/devicons/css3-original.png",
+      iconUrl: "../../assets/devicons/typescript-original.png",
       resourceUrl: "https://www.typescriptlang.org/",
     },
     {
       name: "Node.js",
-      iconUrl: "../../assets/devicons/css3-original.png",
+      iconUrl: "../../assets/devicons/nodejs-original.png",
       resourceUrl: "https://nodejs.org/en/",
     },
     {
+      name: "Express",
+      iconUrl: "../../assets/devicons/express-original.png",
+      resourceUrl: "https://expressjs.com/",
+    },
+    {
       name: "Visual Studio Code",
-      iconUrl: "../../assets/devicons/css3-original.png",
-      resourceUrl: "https://nodejs.org/en/",
+      iconUrl: "../../assets/devicons/Visual_Studio_Code_1.35_icon.png",
+      resourceUrl: "https://code.visualstudio.com/",
+    },
+    {
+      name: "Heroku",
+      iconUrl: "../../assets/devicons/heroku-plain.png",
+      resourceUrl: "https://code.visualstudio.com/",
     },
   ];
   constructor() {}
